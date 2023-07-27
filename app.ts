761d6d50153bef454fbd27f38a1bfe8726e3a14e
HaekyungSeong <haekyungs95@gmail.com>
2023-07-27 15:12:37 +0900

    ts 셋업 설정 완

--- app.ts
+++ app.ts
@@ -1,27 +1,31 @@
-import * as http from "http";
-import { writeHead } from "./models/writeHead";
-import * as fs from "fs";
-import * as path from "path";
+import * as http from 'http';
+import { writeHead } from './models/writeHead';
+import * as fs from 'fs';
+import * as path from 'path';
 
-const app = http
-  .createServer((req, res) => {
-    if (req.method === "GET") {
-      // res.writeHead(200, { "Content-Type": "text/html" });
-      // res.write("???");
-      // res.end();
-      if (req.url === "/") {
-        const page = fs.readFileSync(`${path.join(__dirname, "view", "html", "index.html")}`);
-        writeHead(res, req.url as string, page);
-      } else if (req.url?.includes("favicon")) {
-        const page = "favicon";
+const app = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    // res.writeHead(200, { "Content-Type": "text/html" });
+    // res.write("???");
+    // res.end();
+    if (req.url === '/') {
+      const page = fs.readFileSync(
+        `${path.join(__dirname, 'view', 'html', 'index.html')}`
+      );
+      writeHead(res, req.url as string, page);
+    } else if ((req.url as string).includes('favicon')) {
+      const page = 'favicon';
 
-        writeHead(res, req.url as string, page);
-      } else {
-        const page = fs.readFileSync(`${path.join(__dirname, "view", req.url as string)}`);
-        writeHead(res, req.url as string, page);
-      }
+      writeHead(res, req.url as string, page);
+    } else {
+      const page = fs.readFileSync(
+        `${path.join(__dirname, 'view', req.url as string)}`
+      );
+      writeHead(res, req.url as string, page);
     }
-  })
-  .listen(1101, () => {
-    console.log("port on 1101");
-  });
+  }
+});
+
+app.listen(1101, () => {
+  console.log('port on 1101');
+});
