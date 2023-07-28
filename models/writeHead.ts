86312df681164fdd04d94bfc7e19b86e57acfca8
HaekyungSeong <haekyungs95@gmail.com>
2023-07-28 20:17:22 +0900

    writeHead 모듈 제작

--- models/writeHead.ts
+++ models/writeHead.ts
@@ -1,22 +1,22 @@
-import * as http from "http";
+import type * as http from 'http';
 
-export const writeHead = (res: http.ServerResponse, url: string, page: Buffer | string) => {
-  if (url.endsWith("html") || url.endsWith("/")) {
-    if (url.endsWith("/") || url.includes("favicon")) {
-      res.writeHead(200, { "Content-Type": "text/html" });
+export const writeHead = (res: http.ServerResponse, url: string, page: Buffer | string): void => {
+  if (url.endsWith('html') || url.endsWith('/')) {
+    if (url.endsWith('/') || url.includes('favicon')) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
       res.write(page);
       res.end();
     } else {
-      res.writeHead(200, { "Content-Type": "text/html" });
+      res.writeHead(200, { 'Content-Type': 'text/html' });
       res.write(page);
       res.end();
     }
-  } else if (url.endsWith("css")) {
-    res.writeHead(200, { "Content-Type": "text/css" });
+  } else if (url.endsWith('css')) {
+    res.writeHead(200, { 'Content-Type': 'text/css' });
     res.write(page);
     res.end();
-  } else if (url.endsWith("js")) {
-    res.writeHead(200, { "Content-Type": "text/javascript" });
+  } else if (url.endsWith('js')) {
+    res.writeHead(200, { 'Content-Type': 'text/javascript' });
     res.write(page);
     res.end();
   }
