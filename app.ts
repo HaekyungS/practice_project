5b9c065e77e50c4a8d4e460ca1de5161d1efdc87
HaekyungSeong <haekyungs95@gmail.com>
2023-07-28 20:17:56 +0900

    index.html 및 bundle 파일용으로 서버 수정

--- app.ts
+++ app.ts
@@ -5,22 +5,14 @@ import * as path from 'path';
 
 const app = http.createServer((req, res) => {
   if (req.method === 'GET') {
-    // res.writeHead(200, { "Content-Type": "text/html" });
-    // res.write("???");
-    // res.end();
     if (req.url === '/') {
-      const page = fs.readFileSync(
-        `${path.join(__dirname, 'view', 'html', 'index.html')}`
-      );
+      const page = fs.readFileSync(`${path.join(__dirname, 'dist', 'index.html')}`);
       writeHead(res, req.url as string, page);
     } else if ((req.url as string).includes('favicon')) {
       const page = 'favicon';
-
       writeHead(res, req.url as string, page);
     } else {
-      const page = fs.readFileSync(
-        `${path.join(__dirname, 'view', req.url as string)}`
-      );
+      const page = fs.readFileSync(`${path.join(__dirname, 'dist', req.url as string)}`);
       writeHead(res, req.url as string, page);
     }
   }
