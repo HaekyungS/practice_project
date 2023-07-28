b863a50d7c0603fcceacf31457ee2e8295bc8514
HaekyungSeong <haekyungs95@gmail.com>
2023-07-28 20:17:06 +0900

    테스트용 ts 작성

---
+++ view/index.ts
@@ -0,0 +1,16 @@
+import tagMaker from '../models/tagMaker';
+
+const bodyProps = {
+  style: 'width:100vw; height:100vh',
+};
+
+Object.assign(document.body, bodyProps);
+
+const containerProps = {
+  id: 'container',
+  style:
+    'width:100%; height:100%; display:flex; flexDirection:column; justifyContent:center; alignItems:center; backgroundColor:red',
+  innerText: '아카이로 류 사랑해',
+};
+
+tagMaker('div', document.body, containerProps);
