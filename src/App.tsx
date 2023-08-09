d0b362e081c4461944dbc3da0e6ea07b10f92ad8
HaekyungSeong <haekyungs95@gmail.com>
2023-08-09 13:26:07 +0900

    라우터 적용 오류로 적용안했을 때는 되는지 확인-> 됨

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import { Route, Routes } from 'react-router-dom';
+import { Test } from './view/test';
+
+// export const App: React.FC = () => {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Test />}></Route>
+//     </Routes>
+//   );
+// };
+
+export const App: React.FC = () => {
+  return (
+    <>
+      <Test />
+    </>
+  );
+};
