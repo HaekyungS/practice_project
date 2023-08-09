f456f7939ac892b87ef7b429c509efd691661963
HaekyungSeong <haekyungs95@gmail.com>
2023-08-09 13:26:22 +0900

    entry 파일 작성

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+// import ReactDOM from 'react-dom';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+
+const root = document.getElementById('root');
+
+createRoot(root).render(<App />);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
