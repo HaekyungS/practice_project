23ce445b0a0e34f1703a9069f69199e123c85835
HaekyungSeong <haekyungs95@gmail.com>
2023-08-10 19:16:52 +0900

    test 진행-> 실패

--- src/view/test.tsx
+++ src/view/test.tsx
@@ -3,7 +3,8 @@ import React, { type ReactElement } from 'react';
 export const Test = (): ReactElement => {
   return (
     <div>
-      <div>아카이로 류 사랑해!</div>
+      {/* <div style={{ fontSize: '15px', color: 'blue' }}>아카이로 류 사랑해!</div> */}
+      <div className="text-3xl font-bold underline">아카이로 류 사랑해!</div>
     </div>
   );
 };
