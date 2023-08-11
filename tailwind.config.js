3552d38e9da0c32e59208aaae01c48588d97a6e6
HaekyungSeong <haekyungs95@gmail.com>
2023-08-11 20:34:02 +0900

    기본 테마컬러들 커스텀마이징 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './src/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        main: '#9C2F3A',
+        grey: '#D9D9D9'
+      }
+    }
   },
   plugins: []
 };
