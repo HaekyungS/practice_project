81799d754c77bacf6edacde4d3c604ad491d7371
HaekyungSeong <haekyungs95@gmail.com>
2023-08-11 19:34:56 +0900

    postcss 부분 주석 추가

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: {
+    // tailwindcss 부분은 비워두면 기본값을 적용하고,
+    // 아래와 같이 작성하면 사용자 설정을 적용하여 쓸 수 있다.
     tailwindcss: { config: './tailwind.config.js' },
     autoprefixer: {}
   }
